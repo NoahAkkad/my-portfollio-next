5d4ddb330a59058a8b5e5f854f76b3e2c76a772b
Noah Akkad <noah.akkad@gmail.com>
2024-08-14 23:08:39 +0200

    Add A.jpg

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -16,7 +16,13 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
 }) => {
   return (
     <div className="bg-white shadow-md rounded-lg overflow-hidden">
-      <Image src={image} alt={title} className="w-full h-48 object-cover" />
+      <Image
+        src={image}
+        alt={title}
+        className="w-2/6 h-48 object-cover"
+        width={300}
+        height={200}
+      />
       <div className="p-6">
         <h3 className="text-xl font-semibold text-gray-800 mb-2">{title}</h3>
         <p className="text-gray-600 mb-4">{description}</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import Headers from "./components/Header";
 import Hero from "./components/Hero";
+import ProjectCard from "./components/ProjectCard";
 
 export default function Home() {
   return (
     <>
       <Headers />
       <Hero />
+      <ProjectCard title={"My Portfollio"} description={"fagsdfasd"} image={"/images/A.jpg"} link={""} />
+      <ProjectCard title={"My Portfollio"} description={"fagsdfasd"} image={"/images/A.jpg"} link={""} />
     </>
   );
 }
